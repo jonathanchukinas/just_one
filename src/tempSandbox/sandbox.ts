1a7479bf80c7bccb1dfd68d507d32e6b1475acc5
JonathanChukinas(Maxeta) <JChukinas@maxetatech.local>
2020-04-30 12:39:18 -0400

    set up typescript sandbox

---
+++ src/tempSandbox/sandbox.ts
@@ -0,0 +1,44 @@
+// `tsc sandbox.ts` to compile file to sandbox.js
+// `tsc sandbox.ts -w` to compile file to sandbox.js in realtime
+
+/*
+*******************************************************************************
+From Typescript docs:
+*******************************************************************************
+*/
+
+class Student {
+  fullName: string;
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+
+/*
+*******************************************************************************
+Events
+*******************************************************************************
+*/
+
+
+
+
+
+
+
+
+
+
+document.body.textContent = greeter(user);
