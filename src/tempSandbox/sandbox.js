8a427ce4c8438498b199ef29da941013586bdda0
JonathanChukinas(Maxeta) <JChukinas@maxetatech.local>
2020-04-30 13:38:29 -0400

    typescript sandbox: flavoring

--- src/tempSandbox/sandbox.js
+++ src/tempSandbox/sandbox.js
@@ -12,15 +12,26 @@ var Student = /** @class */ (function () {
         this.lastName = lastName;
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
+    ;
     return Student;
 }());
+;
+;
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
+;
 var user = new Student("Jane", "M.", "User");
-/*
-*******************************************************************************
-Events
-*******************************************************************************
-*/
+;
+var myUuid = '123';
+var myGuid = '456';
+var secondUuid = myGuid;
+var myStr = 'abc';
+function convertToUuid(value) {
+    var newUuid = value;
+    return newUuid;
+}
+console.log(convertToUuid(123));
+;
+var myEvent = { uuidRoom: '123' };
 document.body.textContent = greeter(user);
--- src/tempSandbox/sandbox.ts
+++ src/tempSandbox/sandbox.ts
@@ -1,6 +1,7 @@
 // `tsc sandbox.ts` to compile file to sandbox.js
 // `tsc sandbox.ts -w` to compile file to sandbox.js in realtime
 
+
 /*
 *******************************************************************************
 From Typescript docs:
@@ -11,17 +12,17 @@ class Student {
   fullName: string;
   constructor(public firstName: string, public middleInitial: string, public lastName: string) {
     this.fullName = firstName + " " + middleInitial + " " + lastName;
-  }
-}
+  };
+};
 
 interface Person {
   firstName: string;
   lastName: string;
-}
+};
 
 function greeter(person: Person) {
   return "Hello, " + person.firstName + " " + person.lastName;
-}
+};
 
 let user = new Student("Jane", "M.", "User");
 
@@ -32,7 +33,45 @@ Events
 *******************************************************************************
 */
 
+// https://spin.atomicobject.com/2018/01/15/typescript-flexible-nominal-typing/
 
+// "Factory" for generating new flavors (i.e. custom types): 
+
+interface Flavoring<FlavorT> {
+  _type?: FlavorT;
+};
+type Flavor<T, FlavorT> = T & Flavoring<FlavorT>;
+// type Flavor<T, FlavorT> = T & Flavoring<FlavorT>;
+
+// type UniqueIdentifier = "uuid"
+type UUID = Flavor<string, "UniqueIdentifier">;
+type GUID = Flavor<string, "GloballyUniqueIdentifier">;
+
+const myUuid: UUID = '123'
+const myGuid: GUID = '456'
+const secondUuid: UUID = myGuid
+
+
+const myStr: string = 'abc'
+
+
+function convertToUuid(value: UUID): UUID {
+  const newUuid: UUID = value
+  return newUuid;
+}
+
+console.log(convertToUuid(123))
+
+
+interface gameEvent {
+  uuidRoom: UUID;
+  // uuidPlayer: number;
+  // uuidGame: number;
+  // uuidEvent: number;
+  // idEvent?: number; 
+};
+
+const myEvent = { uuidRoom: '123' }
 
 
 
