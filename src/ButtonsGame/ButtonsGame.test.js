3cdbb73c3c0bc6e97c827baf24e9d490abf6caf0
JonathanChukinas(Maxeta) <JChukinas@maxetatech.local>
2020-04-29 21:05:32 -0400

    group tests in 'describe' blocks

--- src/ButtonsGame/ButtonsGame.test.js
+++ src/ButtonsGame/ButtonsGame.test.js
@@ -2,44 +2,47 @@ import buttonsGameMachine from './ButtonsGame.machine';
 import { interpret } from 'xstate';
 
 
-const { initialState } = buttonsGameMachine;
+describe('test buttons game transitions', () => {
 
+  const { initialState } = buttonsGameMachine;
 
-test('buttons game machine initial state', () => {
-  expect(
-    initialState.value
-  ).toEqual('idle');
+  test('initial state', () => {
+    expect(initialState.value).toEqual('idle');
+  });  
+  
+  test('START_GAME', () => {
+    const turn1 = buttonsGameMachine.transition(initialState, 'START_GAME');
+    expect(turn1.value).toEqual('playRound');
+  });
+
 });
 
 
-test('button machine onToggle', () => {
-  expect(
-    buttonsGameMachine.transition(initialState, 'START_GAME').value
-  ).toEqual('playRound');
-});
-
-
-test('buttons game machine, interpreted', () => {
+describe('run an interpreted buttons game', () => {
 
   // Start the game
   const buttonsGameService = interpret(buttonsGameMachine)
   buttonsGameService.start();
-  const turn1 = buttonsGameService.send('START_GAME');
-
-  // Turn 1
-  expect(turn1.value).toBe('playRound');
-  expect(turn1.context.roundNum).toEqual(1);
   
-  // Turn 2
-  const turn2 = buttonsGameService.send('END_ROUND');
-  expect(turn2.context.roundNum).toEqual(2);
-  
-  // Turn 3
-  const turn3 = buttonsGameService.send('END_ROUND');
-  expect(turn3.context.roundNum).toEqual(3);
+  test('Advance to Turn 1', () => {
+    const turn1 = buttonsGameService.send('START_GAME');
+    expect(turn1.value).toBe('playRound');
+    expect(turn1.context.roundNum).toEqual(1);
+  })
 
-  // End Game
-  const endGameState = buttonsGameService.send('END_ROUND');
-  expect(endGameState.done).toBe(true);
+  test('Advance to Turn 2', () => {
+    const turn2 = buttonsGameService.send('END_ROUND');
+    expect(turn2.context.roundNum).toEqual(2);
+  })
+
+  test('Advance to Turn 3', () => {
+    const turn3 = buttonsGameService.send('END_ROUND');
+    expect(turn3.context.roundNum).toEqual(3);
+  })
+
+  test('End Game', () => {
+    const endGameState = buttonsGameService.send('END_ROUND');
+    expect(endGameState.done).toBe(true);
+  })
   
 })
