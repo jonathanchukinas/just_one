c843ab81fa4c1813a4acf24f6de6299cbd220637
JonathanChukinas(Maxeta) <JChukinas@maxetatech.local>
2020-05-04 19:47:52 -0400

    draft buttons game machine

--- src/ButtonsGame/components/ButtonsGame.machine.js
+++ src/ButtonsGame/components/ButtonsGame.machine.js
@@ -1,27 +1,38 @@
-import { Machine, assign } from 'xstate';
+import { Machine, assign, spawn } from 'xstate';
+import buttonMachine from './Button.machine';
 
 // TODO naming convention for id?
 const buttonsGameMachine = Machine({
-  id: 'buttonsGame',
+  id: 'game',
   context: {
-    roundNum: 0,
-    playerCount: 0,
-    finalRoundNum: 3,
-    playerNames: [],
+    roundCount: 0,
+    players: {},
   },
   initial: 'idle',
   states: {
     idle: {
       on: {
-        START_GAME: 'playRound',
+        START_GAME: 'round',
       }
     },
-    playRound: {
-      entry: 'incrementRoundNum',
+    round: {
+      entry: 'incrementRoundCount',
       on: {
-        END_ROUND: [
-          { target: 'endGame', cond: 'isEndGame' },
-          { target: 'playRound' },
+        CHECK_ROUND_END: {
+          target: 'resolution',
+          cond: 'areAllButtonsComplete'
+        }
+      }
+    },
+    resolution: {
+      on: {
+        "": [
+          {
+            target: 'endGame',
+            cond: 'isFinalRound'
+          },{
+            target: 'round'
+          },
         ]
       }
     },
@@ -36,19 +47,31 @@ const buttonsGameMachine = Machine({
   }
 },{
   actions: {
-    incrementRoundNum: context => context.roundNum++,
-    addPlayer: assign((context, event)=>{
-      console.log('Player added!', event.playerName)
-      const { playerNames } = context
-      playerNames.push(event.playerName)
-      return {
-        ...context,
-        playerNames: playerNames,
+    incrementRoundCount: assign({ roundCount: context => context.count + 1 }),
+    addPlayer: assign({
+      players: (context, event) => {
+        const playerName = event.playerName
+        const newButtonMachineContext = { playerName }
+        const players = context.players.concat({
+          [playerName]: spawn(
+            buttonMachine.withContext(newButtonMachineContext),
+            { sync: true }
+          )
+        })
+        return players
       }
     }),
   },
   guards: {
-    isEndGame: context => (context.roundNum == context.finalRoundNum),
+    areAllButtonsComplete: ctx => {
+      ctx.players.keys().forEach(playerRef => {
+        const playerMachine = ctx.players[playerRef]
+        const playerState = playerMachine.states
+        if (playerState != 'complete') { return false}
+      })
+      return true
+    },
+    isEndGame: context => (context.roundCount == context.finalRoundNum),
   }
 });
 
