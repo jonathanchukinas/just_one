c788e5b757b03906285529aab1b78cced4d5bce9
JonathanChukinas(Maxeta) <JChukinas@maxetatech.local>
2020-05-04 21:34:52 -0400

    buttons render

--- src/ButtonsGame/components/Button.js
+++ src/ButtonsGame/components/Button.js
@@ -5,33 +5,38 @@ import { useService } from '@xstate/react';
 export default function Button({ machine }) {
 
   const [state, send] = useService(machine);
-  // const currentState = state.value;
   const playerName = state.context.playerName;
   // const isSelf = state.context.isSelf;
   // const handleClick = ()=> {if (isSelf) { send('TOGGLE') }}
+  const handleClick = ()=> {send('TOGGLE')}
+
+  console.log(playerName, state)
 
 
-  // const colors = {
-  //   incomplete: 'red',
-  //   complete: 'green'
-  // };
-  // const buttonColor = colors[currentState];
+  const colors = {
+    pending: 'red',
+    complete: 'green'
+  };
+  const currentState = state.value
+  const buttonColor = colors[currentState];
 
 
   // const buttonSaturation = (
   //   isSelf ? 
   //   {bg: 400, text: 900} : {bg: 100, text: 300}
   // );
-  // const cssBgColor = `bg-${buttonColor}-${buttonSaturation.bg}`
-  // const cssTextColor = `text-${buttonColor}-${buttonSaturation.text}`
-  // const cssBorder = `border-2 border-${buttonColor}-${buttonSaturation.text}`;
+  const buttonSaturation = {bg: 400, text: 900}
+  const cssBgColor = `bg-${buttonColor}-${buttonSaturation.bg}`
+  const cssTextColor = `text-${buttonColor}-${buttonSaturation.text}`
+  const cssBorder = `border-2 border-${buttonColor}-${buttonSaturation.text}`;
   // const cssHoverColor = (isSelf ? `hover:bg-${buttonColor}-500` : "");
-  // const cssNotSelf = (isSelf ? "" : "cursor-default");
-  // const buttonCss = `w-56 font-bold py-2 px-4 rounded-full ${cssBgColor} ${cssTextColor} ${cssHoverColor} ${cssBorder} ${cssNotSelf}`
+  const cssHoverColor = `hover:bg-${buttonColor}-500`;
+  const cssNotSelf = "";
+  const buttonCss = `w-56 font-bold py-2 px-4 rounded-full ${cssBgColor} ${cssTextColor} ${cssHoverColor} ${cssBorder} ${cssNotSelf}`
   
   return (<div className="mt-6" >
-    <p>{playerName}</p>
-    {/* <button className={buttonCss} onClick={ handleClick } >{playerName}</button> */}
+    {/* <p>{playerName}</p> */}
+    <button className={buttonCss} onClick={ handleClick } >{playerName}</button>
   </div>);
 
 }
